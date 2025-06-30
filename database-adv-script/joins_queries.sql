60354ad22432c5b23d09ecb9f00a8062d642b6d0
AJ Iyanu <aderemijoseph@live.com>
2025-06-30 13:29:15 +0100

    Refactor SQL join query for properties and reviews to include ordering by rating and creation date

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,9 +2,16 @@ SELECT bookings.*, users.*
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
-SELECT properties.*, reviews.*
-FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+SELECT
+    properties.*,
+    reviews.*
+FROM
+    properties
+LEFT JOIN
+    reviews ON properties.property_id = reviews.property_id
+ORDER BY
+    reviews.rating DESC NULLS LAST,
+    reviews.created_at DESC;
 
 SELECT users.*, bookings.*
 FROM users
