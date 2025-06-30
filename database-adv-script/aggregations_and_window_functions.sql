4cb33b79b42ac61871120452f3929396d7c11ed3
AJ Iyanu <aderemijoseph@live.com>
2025-06-30 13:49:26 +0100

    added row number window function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -31,3 +31,24 @@ SELECT
     RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
 FROM 
     property_booking_counts;
+
+
+WITH property_booking_counts AS (
+    SELECT 
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        properties p
+    LEFT JOIN 
+        bookings b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id, p.name
+)
+SELECT 
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM 
+    property_booking_counts;
